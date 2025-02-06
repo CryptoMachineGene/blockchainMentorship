c46227cfe77225516f63fd9dba75bd3d259c86b8
cplKlegg <--global>
2025-02-06 00:51:15 -0500

    3. Testing Contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+//import helpers from "@nomicfoundation/hardhat-toolbox/network-helpers";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
