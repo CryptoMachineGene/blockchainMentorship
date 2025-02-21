8a5d358adae4f44f92320c881d53484f1623a4cb
cplKlegg <emcgrath@startmail.com>
2025-02-20 21:19:46 -0500

    8. Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 //import helpers from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
@@ -7,6 +8,10 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   },
 };
